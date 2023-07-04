77528e34ade1db0cc2e78b24ed41666bccc60947
serethewind <osasereu@gmail.com>
2023-07-04 10:46:29 +0100

    Cors bean added

--- src/main/java/com/hackathon/ehealthcareproject/EHealthcareProjectApplication.java
+++ src/main/java/com/hackathon/ehealthcareproject/EHealthcareProjectApplication.java
@@ -4,9 +4,26 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class EHealthcareProjectApplication {
+
+	@Bean
+	public WebMvcConfigurer webMvcConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+
+				registry
+						.addMapping("/**")
+						.allowedOrigins("*","")
+						.allowedHeaders("*")
+						.allowedMethods("POST","GET","PUT","PATCH","OPTIONS");
+			}
+		};
+	}
 
 	@Bean
 	public ModelMapper modelMapper(){
